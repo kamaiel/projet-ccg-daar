ea3b17c53a9a0be9d3562221086fd0b8b7733c72
Farouck <3407463>
2023-10-27 12:16:35 +0200

    Deployement to debug

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const axios = require('axios')
 const app = express()
@@ -7,12 +8,14 @@ const API_key = "43aadbec-ef33-49cb-abcc-1a3810dd598f"
 
 /* ---------------------------------------------------------------------- */
 
-const ethers = require('ethers')
+const {ethers}= require('ethers')
 /*const { Web3 } = require('web3')
 const provider = 'http://127.0.0.1:8545'
 const web3 = new Web3(provider)*/
 const ABI = require('../contracts/artifacts/src/Main.sol/Main.json')
 const abi = ABI.abi
+
+const contracts = require('../frontend/src/contracts.json')
 
 const bytecodeabi = ABI.bytecode
 
@@ -60,36 +63,6 @@ const adress_collec1 = {}*/
 
 /* -------------------- Appels Blockchain ----------------------- */
 
-async function deployContract() {
-    const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8545'); 
-    const signer = await provider.getSigner(); 
-  
-    try {
-      const factory = new ethers.ContractFactory(abi, bytecodeabi, signer);
-      const contract = await factory.deploy();
-      console.log(contract)
-      await contract.deployTransaction();
-  
-      console.log('Contrat déployé avec succès à l\'adresse :', contract.address);
-  
-     
-  
-    } catch (error) {
-      console.error('Erreur lors du déploiement du contrat :', error);
-    }
-  }
-
-async function creationCollection (){
-    try {
-        for(var i = 0 ; i < 6 ; i ++){
-            const toSign = await MainContract.methods.createCollection(metaDataSet.data[i].name,Number(metaDataSet.data[i].total)).call()
-            
-           }
-        console.log("Collection creation was created with sucess")
-    }catch(error){
-        console.log("Error with collection creation\n", error)
-    }
-}
 
 /* ---------------------------------------------------------------------- */
 
@@ -105,3 +78,4 @@ app.listen(port, async () => {
     await deployContract()
     creationCollection()
 })
+
--- contracts/deploy/000001-deploy-game.ts
+++ contracts/deploy/000001-deploy-game.ts
@@ -1,10 +1,36 @@
 import 'dotenv/config'
 import { DeployFunction } from 'hardhat-deploy/types'
+import contracts from '../../frontend/src/contracts.json'
+import ethers, { ContractFactory } from 'ethers'
+import bytecode from '../artifacts/src/Main.sol/Main.json'
+
 
 const deployer: DeployFunction = async hre => {
   if (hre.network.config.chainId !== 31337) return
   const { deployer } = await hre.getNamedAccounts()
   await hre.deployments.deploy('Main', { from: deployer, log: true })
+
+  /* ---------------------------------------------------------------------- */
+  /* ---------------Création des collections par le superAdmin-------------- */
+  /* ---------------------------------------------------------------------- */
+
+
+  const {address, abi} = contracts.contracts.Main
+  const provider = await hre.ethers.getContractFactory('Main')
+  const signer = provider.signer
+  try {
+    const factory = new ContractFactory(abi, bytecode.bytecode, signer);
+    const contract = await factory.deploy();
+    await contract.deployed()
+    const contract_ 
+    
+    console.log("ContractMain Collection create OK")
+  }catch(error){
+    console.error('Erreur lors du déploiement du contrat :', error);
+
+  }
+
+
   
 }
 
